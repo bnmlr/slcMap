90d945559101d53ebbec014c369f0559acbcf623
Ben Miller <Ben Miller>
2016-06-08 19:51:34 -0600

    Add map marker

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-    zoom: 8
+    var myLatlng = {lat: -34.397, lng: 150.644};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatlng,
+        zoom: 8
+    });
+
+    var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Click to zoom'
   });
+
 }
