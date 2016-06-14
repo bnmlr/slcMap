e57dac25fc4306fda0ed626293612246a8287b58
Ben Miller <Ben Miller>
2016-06-13 21:17:20 -0600

    Add model and viewmodel structure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,41 @@
 
+//code organization based on Heidi Kasemir gist
+//model
+var data = [{
+    name: "Liberty Park"
+    pos: "of"
+    },{
+    name: "Pioneer Park"
+    pos: "objects"
+}];
+
+var PlaceConstructor = function(dataObj){
+  this.name = ko.observable(dataObj.name)
+  // ...etc...
+  // this includes all the things you want to track per location (whether it's a 'favorite' or not?)
+}
+
+var ViewModel = function() {
+    var self = this;
+
+    self.locationsArray = ko.observableArray();
+
+    data.forEach(function(location) {
+    self.arrayOfAllMyLocations.push(new PlaceConstructor(location));
+    // you can also make all the markers here
+    
+    })
+
+    self.createMarkers = function() {
+    // make the markers, set event listeners, get infowindow content, etc
+    // this may be pieced out into as many smaller functions as you think make sense
+    }
+
+     // you will probably need to make additional functions to make this code modular and clean
+  // or to add your unique functionality
+
+}
+
 function initMap() {
     var myLatlng = {lat: -34.397, lng: 150.644};
 
