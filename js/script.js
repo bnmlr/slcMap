e173913b23ce51e3314a4bccce448b5b6fc3caa2
Ben Miller <Ben Miller>
2016-06-15 22:40:28 -0600

    Add data names to viewmodel and data bindings

--- js/script.js
+++ js/script.js
@@ -1,47 +1,52 @@
 
 //code organization based on Heidi Kasemir gist
 //model
-var data = [{
-    name: "Liberty Park"
-    pos: "of"
-    },{
-    name: "Pioneer Park"
-    pos: "objects"
-}];
+// var data = [{
+//     name: "Liberty Park",
+//     pos: {lat: 40.747002, lng: -111.874150}
+//     },{
+//     name: "Pioneer Park",
+//     pos: {lat: 40.761771, lng: -111.901073}
+// }];
 
-var PlaceConstructor = function(dataObj){
-  this.name = ko.observable(dataObj.name)
-  // ...etc...
-  // this includes all the things you want to track per location (whether it's a 'favorite' or not?)
-}
+// var PlaceConstructor = function(dataObj){
+//   this.name = ko.observable(dataObj.name)
+//   // ...etc...
+//   // this includes all the things you want to track per location (whether it's a 'favorite' or not?)
+// }
 
 var ViewModel = function() {
     var self = this;
+    self.placeArray = ko.observableArray(['Liberty Park', 'Pioneer Park']);
 
-    self.locationsArray = ko.observableArray();
 
-    data.forEach(function(location) {
-    self.arrayOfAllMyLocations.push(new PlaceConstructor(location));
-    // you can also make all the markers here
+  //   self.locationsArray = ko.observableArray();
+
+  //   data.forEach(function(location) {
+  //   self.arrayOfAllMyLocations.push(new PlaceConstructor(location));
+  //   // you can also make all the markers here
     
-    })
+  //   })
 
-    self.createMarkers = function() {
-    // make the markers, set event listeners, get infowindow content, etc
-    // this may be pieced out into as many smaller functions as you think make sense
-    }
+  //   self.createMarkers = function() {
+  //   // make the markers, set event listeners, get infowindow content, etc
+  //   // this may be pieced out into as many smaller functions as you think make sense
+  //   }
 
-     // you will probably need to make additional functions to make this code modular and clean
-  // or to add your unique functionality
+  //    // you will probably need to make additional functions to make this code modular and clean
+  // // or to add your unique functionality
 
 }
 
+ko.applyBindings(new ViewModel());
+
+
 function initMap() {
-    var myLatlng = {lat: -34.397, lng: 150.644};
+    var myLatlng = {lat: 40.761353, lng: -111.891851};
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: myLatlng,
-        zoom: 8
+        zoom: 13
     });
 
     var marker = new google.maps.Marker({
