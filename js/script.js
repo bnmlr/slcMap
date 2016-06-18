4f9d670460dd144ded0c5ec83fd92fee28e05527
Ben Miller <Ben Miller>
2016-06-18 10:35:50 -0600

    Create list of places based on observable array

--- js/script.js
+++ js/script.js
@@ -1,40 +1,37 @@
 
 //code organization based on Heidi Kasemir gist
 //model
-// var data = [{
-//     name: "Liberty Park",
-//     pos: {lat: 40.747002, lng: -111.874150}
-//     },{
-//     name: "Pioneer Park",
-//     pos: {lat: 40.761771, lng: -111.901073}
-// }];
+var data = [{
+    name: "Liberty Park",
+    pos: {lat: 40.747002, lng: -111.874150}
+    },{
+    name: "Pioneer Park",
+    pos: {lat: 40.761771, lng: -111.901073}
+}];
 
-// var PlaceConstructor = function(dataObj){
-//   this.name = ko.observable(dataObj.name)
-//   // ...etc...
-//   // this includes all the things you want to track per location (whether it's a 'favorite' or not?)
-// }
+var PlaceConstructor = function(dataObj){
+  this.name = ko.observable(dataObj.name)
+  this.pos = ko.observable(dataObj.pos)
+  // ...etc...
+  // this includes all the things you want to track per location (whether it's a 'favorite' or not?)
+}
 
 var ViewModel = function() {
     var self = this;
-    self.placeArray = ko.observableArray(['Liberty Park', 'Pioneer Park']);
+    self.arrayOfAllMyLocations = ko.observableArray();
+    data.forEach(function(location) {
+    self.arrayOfAllMyLocations.push(new PlaceConstructor(location));
+    // you can also make all the markers here
 
+    })
 
-  //   self.locationsArray = ko.observableArray();
+    self.createMarkers = function() {
+    // make the markers, set event listeners, get infowindow content, etc
+    // this may be pieced out into as many smaller functions as you think make sense
+    }
 
-  //   data.forEach(function(location) {
-  //   self.arrayOfAllMyLocations.push(new PlaceConstructor(location));
-  //   // you can also make all the markers here
-    
-  //   })
-
-  //   self.createMarkers = function() {
-  //   // make the markers, set event listeners, get infowindow content, etc
-  //   // this may be pieced out into as many smaller functions as you think make sense
-  //   }
-
-  //    // you will probably need to make additional functions to make this code modular and clean
-  // // or to add your unique functionality
+     // you will probably need to make additional functions to make this code modular and clean
+  // or to add your unique functionality
 
 }
 
