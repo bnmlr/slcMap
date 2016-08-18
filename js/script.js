885ebf5befcc2d29c5e725cd706597a7792a6aac
Ben Miller <Ben Miller>
2016-08-17 23:42:35 -0600

    Attempt to replace hardcoded model w/ foursquare data. add comments about issues.

--- js/script.js
+++ js/script.js
@@ -1,53 +1,67 @@
 
 //code organization based on Heidi Kasemir gist w/ additional help from Ryan Vrba
 
-// var fourSquareURL = 'https://api.foursquare.com/v2/venues/search?near=%22salt%20lake%20city,%20ut%22&limit=10&radius=8046.72&categoryId=4bf58dd8d48988d163941735&client_id=N4151NYLOJ3FQ0GYHUZ4O0OTKNAKX3NW2PJY1HH2503G35WU&client_secret=ALHWZESIYI1MWFX51A0FEKDWNTAKJNFQFHISRSJZM1TUZTAD&v=20160812'
-// $.getJSON(URL, function(data) {
-//     console.log(data);
-// });
+//NOTE: code is a mess right now. in the middle of trying to replace hardcoded model
+// with data from foursquare. 
+//Ultimately  I'd like the app to show a map of top 10 parks 
+//in a given city using data from foursquare. Salt Lake City would be the default. 
+//User could then change the city.
+//The map and filtering worked when i had a hardcoded model. Issue I ran into when I added 
+//foursquare data was that request wasn't getting back in time to populate model before
+//building the vm. So now I'm trying to not create the vm until callback, but that's 
+//causing problems w/ init map. tried to initMap in callback too, but wouldn't work.
+//Seems like trying to make everything wait until ajax response is returned defeats the purpose
+//of an asynchronous request, but I'm not sure how else to get the data.
 
-//example
-// https://api.foursquare.com/v2/venues/search
-//   ?client_id=CLIENT_ID
-//   &client_secret=CLIENT_SECRET
-//   &v=20130815
-//   &ll=40.7,-74
-//   &query=sushi
+var fourSquareURL = 'https://api.foursquare.com/v2/venues/search?near=%22salt%20lake%20city,%20ut%22&limit=10&radius=8046.72&categoryId=4bf58dd8d48988d163941735&client_id=N4151NYLOJ3FQ0GYHUZ4O0OTKNAKX3NW2PJY1HH2503G35WU&client_secret=ALHWZESIYI1MWFX51A0FEKDWNTAKJNFQFHISRSJZM1TUZTAD&v=20160812'
+var placeData = [];
 
-//url for pioneer park
-//https://api.foursquare.com/v2/venues/search?ll=40.761771,-111.901073&limit=1&client_id=N4151NYLOJ3FQ0GYHUZ4O0OTKNAKX3NW2PJY1HH2503G35WU&client_secret=ALHWZESIYI1MWFX51A0FEKDWNTAKJNFQFHISRSJZM1TUZTAD&v=20160812
+ 
+$.getJSON(fourSquareURL, function(data) {
+    placeData = data.response.venues
+    //trying to initialize the viewodel in the success callback to make sure data is recieved before vm is built
+    console.log("1");
+    vm = new ViewModel();
+    ko.applyBindings(vm);
+});
+
 
 //model
-var data = [{
-    name: "Liberty Park",
-    pos: {lat: 40.747002, lng: -111.874150}
-    },{
-    name: "Pioneer Park",
-    pos: {lat: 40.761771, lng: -111.901073}
-}];
+// var data = [{
+//     name: "Liberty Park",
+//     pos: {lat: 40.747002, lng: -111.874150}
+//     },{
+//     name: "Pioneer Park",
+//     pos: {lat: 40.761771, lng: -111.901073}
+// }];
 
-var map,
+//in function below, replaced data w/ placeData
+
+
+var vm,
+    map,
     infowindow,
     bounds;
 
 var PlaceConstructor = function(dataObj){
   this.name = ko.observable(dataObj.name)
-  this.pos = ko.observable(dataObj.pos)
-  // ...etc...
-  // this includes all the things you want to track per location (whether it's a 'favorite' or not?)
+  //wil have to update marker function now that lat and lng are separate
+  this.pos = ko.observable(dataObj.location.lat)
+  this.lng = ko.observable(dataObj.location.lng)
+  this.address = ko.observable(dataObj.location.address)
 }
 
 var ViewModel = function() {
+    console.log("2");
     var self = this;
     self.arrayOfAllMyLocations = ko.observableArray();
-    data.forEach(function(location) {
+    placeData.forEach(function(location) {
         self.arrayOfAllMyLocations.push(new PlaceConstructor(location));
     })
     
     //took function from 
     //https://discussions.udacity.com/t/adding-click-event-to-list-item-and-open-infowindow/177224/3
     self.listItemClick = function(marker) {
-    console.log(marker);
     google.maps.event.trigger(this.marker, 'click');
     };
     //controls marker click behavior. goal was to have one function that marker and list clicks could both
@@ -64,9 +78,11 @@ var ViewModel = function() {
 
     //define marker creation function here, call it inside initmap
     self.createMarkers = function() {
+        console.log("3");
         for (var i = 0; i < vm.arrayOfAllMyLocations().length; i++) {
             var marker = new google.maps.Marker({
-            position: vm.arrayOfAllMyLocations()[i].pos(),
+                //will need to update position now that lat and lng are separate, no more sing pos
+            position: vm.arrayOfAllMyLocations()[i].pos(), 
             map: map,
             title: vm.arrayOfAllMyLocations()[i].name()
             });
@@ -117,10 +133,12 @@ var ViewModel = function() {
 };
 
 
-var vm = new ViewModel();
-ko.applyBindings(vm);
 
+
+//issue I'm running into is that map depends on vm which isn't built in time.
+//tried to initialize map in ajax success callback but it wasn't working.
 function initMap() {
+    console.log("4");
     map = new google.maps.Map(document.getElementById('map'));
  //create one infowindow and just switch out the content on clicks   
     infowindow = new google.maps.InfoWindow({
@@ -136,20 +154,3 @@ function initMap() {
     });
     map.fitBounds(bounds);
 };
-
-
-// var fourSquareURL = 'https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=N4151NYLOJ3FQ0GYHUZ4O0OTKNAKX3NW2PJY1HH2503G35WU&client_secret=ALHWZESIYI1MWFX51A0FEKDWNTAKJNFQFHISRSJZM1TUZTAD&v=20160812'
-// $.getJSON(URL, function(data) {
-//     console.log(data);
-// });
-
-//example
-// https://api.foursquare.com/v2/venues/search
-//   ?client_id=CLIENT_ID
-//   &client_secret=CLIENT_SECRET
-//   &v=20130815
-//   &ll=40.7,-74
-//   &query=sushi
-
-//url for pioneer park
-//https://api.foursquare.com/v2/venues/search?ll=40.761771,-111.901073&limit=1&client_id=N4151NYLOJ3FQ0GYHUZ4O0OTKNAKX3NW2PJY1HH2503G35WU&client_secret=ALHWZESIYI1MWFX51A0FEKDWNTAKJNFQFHISRSJZM1TUZTAD&v=20160812
