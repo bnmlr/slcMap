f2dbee67dff7b3bf11bc86a0ee4348b2538a31f2
Ben Miller <Ben Miller>
2016-08-20 10:44:06 -0600

    Add fallback for missing addresses and clean out old comments

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 
 //code organization based on Heidi Kasemir gist w/ additional help from Ryan Vrba
 
-var fourSquareURL = 'https://api.foursquare.com/v2/venues/search?near=%22salt%20lake%20city,%20ut%22&limit=10&radius=8046.72&categoryId=4bf58dd8d48988d163941735&client_id=N4151NYLOJ3FQ0GYHUZ4O0OTKNAKX3NW2PJY1HH2503G35WU&client_secret=ALHWZESIYI1MWFX51A0FEKDWNTAKJNFQFHISRSJZM1TUZTAD&v=20160812'
+var fourSquareURL = 'https://api.foursquare.com/v2/venues/search?near=%22salt%20lake%20city,%20ut%22&limit=5&radius=8046.72&categoryId=4bf58dd8d48988d163941735&client_id=N4151NYLOJ3FQ0GYHUZ4O0OTKNAKX3NW2PJY1HH2503G35WU&client_secret=ALHWZESIYI1MWFX51A0FEKDWNTAKJNFQFHISRSJZM1TUZTAD&v=20160812'
 var placeData = [];
-
- 
+//Get foursquare data
 $.getJSON(fourSquareURL, function(data) {
     placeData = data.response.venues;
-    // Create map markers using location data
-    vm.createMarkers(placeData);
+    //Create loations once foursquare data is received
+    vm.createLocations(placeData);
+    //Set bounds of map to markers
     bounds = new google.maps.LatLngBounds();
     vm.arrayOfAllMyLocations().forEach(function(item) {
         bounds.extend(item.marker.getPosition());
@@ -16,33 +16,25 @@ $.getJSON(fourSquareURL, function(data) {
     map.fitBounds(bounds);
 });
 
-
-//model
-// var data = [{
-//     name: "Liberty Park",
-//     pos: {lat: 40.747002, lng: -111.874150}
-//     },{
-//     name: "Pioneer Park",
-//     pos: {lat: 40.761771, lng: -111.901073}
-// }];
-
-//in function below, replaced data w/ placeData
-
-
 var vm,
     map,
     infowindow,
     bounds;
 
-var PlaceConstructor = function(dataObj) {
+//Builds out each location with data from foursquare
+var LocationConstructor = function(dataObj) {
     this.name = dataObj.name;
     this.pos = {lat: dataObj.location.lat, lng: dataObj.location.lng};
+    if (typeof dataObj.location.address === "undefined") {
+        dataObj.location.address = "Not available";
+    };
     this.address = dataObj.location.address;
+    //creates a marker for each location
     var marker = new google.maps.Marker({
-                //will need to update position now that lat and lng are separate, no more sing pos
                 position: {lat: dataObj.location.lat, lng: dataObj.location.lng}, 
                 map: map,
-                title: dataObj.name
+                title: dataObj.name,
+                content: dataObj.name + "<br>" + "Address: " + dataObj.location.address
             });
             marker.addListener('click', vm.markerClick);
     this.marker = marker;
@@ -51,18 +43,14 @@ var PlaceConstructor = function(dataObj) {
 var ViewModel = function() {
     var self = this;
     self.arrayOfAllMyLocations = ko.observableArray();
-    /*placeData.forEach(function(location) {
-        self.arrayOfAllMyLocations.push(new PlaceConstructor(location));
-    })*/
-    
     //took function from 
     //https://discussions.udacity.com/t/adding-click-event-to-list-item-and-open-infowindow/177224/3
     self.listItemClick = function(marker) {
         google.maps.event.trigger(this.marker, 'click');
     };
-    //controls marker click behavior. goal was to have one function that marker and list clicks could both
-    //trigger, but couldn't figure it out, so i created a separate one, listItemClick
+    //controls marker click behavior
     self.markerClick = function () {
+        infowindow.setContent(this.content);
         infowindow.open(map, this);
         //marker bounces once on click
         this.setAnimation(google.maps.Animation.BOUNCE);
@@ -72,38 +60,15 @@ var ViewModel = function() {
         }, 700);
     };
 
-    //define marker creation function here, call it inside initmap
-    self.createMarkers = function(data) {
+    //function creates locations and builds observable array of locations. initialized on ajax success
+    self.createLocations = function(data) {
         for (var i = 0; i < data.length; i++) {
-            console.log(data[i]);
-
-
-            // Create a new PlaceConstructor object for each Foursquare result
-            // And push the object to the 'self.arrayOfAllMyLocations' observable array
-            self.arrayOfAllMyLocations.push(new PlaceConstructor(data[i]));
-
-            // Create a Map Marker using the PlaceConstructor object
-            // You could also create the map marker directly inside the PlaceConstructor function
-
-            /*var marker = new google.maps.Marker({
-                //will need to update position now that lat and lng are separate, no more sing pos
-                position: vm.arrayOfAllMyLocations()[i].pos(), 
-                map: map,
-                title: vm.arrayOfAllMyLocations()[i].name()
-            });
-            marker.addListener('click', vm.markerClick);
-
-            /*
-             * I recommend creating the markers before pushing the locations to the observable array
-             * One method is creating the markers inside the PlaceConstructor function
-             * Another would be creating the PlaceConstructor location object, then creating the marker, then pushing
-             * the location object once you are finished with all data manipulation
-             */
+            self.arrayOfAllMyLocations.push(new LocationConstructor(data[i]));
         }
     }; 
 
     self.filter = ko.observable("");
-    // filter the items using the filter text
+    // filters the items using the filter text
     // list is bound to self.filteredItems, which updates based on filter input
     self.filteredItems = ko.computed(function() {
 
@@ -119,9 +84,10 @@ var ViewModel = function() {
             };
             return self.arrayOfAllMyLocations();
         } else {
-            //function allows us to pass in an array and control which items are 
+            //ko array filter function allows us to pass in an array and control which items are 
             //included in a new array based on the result of the function executed 
-            //on each item of the original array 
+            //on each item of the original array
+
             return ko.utils.arrayFilter(self.arrayOfAllMyLocations(), function(item) {
                 //compares name of each item in array to the
                 //string in the filter. if it's a match, it makes
@@ -129,13 +95,12 @@ var ViewModel = function() {
                 //gets passed through to the filtered array. if it's not a match, the marker
                 //is set to invisible and item isn't passed on to filtered array
 
-              if (item.name().toLowerCase().indexOf(filter) !== -1) {
-              //if (stringStartsWith(item.name().toLowerCase(), filter)) {
-                    //not sure line below is necessary
+              if (item.name.toLowerCase().indexOf(filter) !== -1) {
                     item.marker.setVisible(true);
                     return true
                } else {
                     item.marker.setVisible(false);
+                    infowindow.close();
                     return false
                }
             });
@@ -156,9 +121,4 @@ function initMap() {
     // Create ViewModel and apply Knockout bindings
     vm = new ViewModel();
     ko.applyBindings(vm);
-
-    //set the bounds of the map to wherever the markers are
-    // You might want to initialize the bounces object in the success function of the Foursquare request instead
-    // This way, it will be reset each time new locations are loaded
-    
 };
